992c77f8c8edb96be2840070d77e74684ef9edbe
yalisun <zixiao.yali@gmail.com>
2021-04-06 14:20:39 -0700

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses/',
   headers: {
     Authorization: 
-      'Bearer vinpiGZdAH7PIWg6h0xLUCUrS2s2wm0OkChPIQq7t_w_OrEFqaxil8FkRYRA29_qeQK-ba1k_DanCnj1Xumjc1mXOQNgh0Y2GGqFqrQFUqFcuF3qT0feEsQ0OM9sYHYx'
+      'Bearer my-api-key'
   }
 });
